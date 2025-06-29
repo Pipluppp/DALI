97b2713856ebb6fb59b7e62c972de266f40f18e5
dan can <70219682+Pipluppp@users.noreply.github.com>
2025-06-29 20:12:34 +0800

    REFACTOR BACKEND TO PACKAGE BY FEATURE
    This is easier for us to map out the files by their feature instead of their type.

---
+++ src/main/java/com/dali/ecommerce/cart/CartItemRepository.java
@@ -0,0 +1,14 @@
+// DALI/src/main/java/com/dali/ecommerce/repository/CartItemRepository.java
+package com.dali.ecommerce.cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
+    List<CartItem> findByAccountAccountId(Integer accountId);
+    Optional<CartItem> findByAccountAccountIdAndProductId(Integer accountId, Integer productId);
+    void deleteByAccountAccountId(Integer accountId);
+}
--- src/main/java/com/dali/ecommerce/maya/MayaService.java
+++
@@ -1,8 +0,0 @@
-package com.dali.ecommerce.maya;
-
-import com.dali.ecommerce.maya.CheckoutResponse;
-import com.dali.ecommerce.model.Order;
-
-public interface MayaService {
-    CheckoutResponse createCheckout(Order order) throws Exception;
-}
